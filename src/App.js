504695fcefe85f65a2bb989a3897e01d8ead2786
akshitchowdhury <akshitchowdhury@gmai.com>
2023-04-25 20:06:54 +0530

    app2

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+    
+{/* <Navbar title = 'Shura-bar' scan = "Scan">
+
+</Navbar> */}
+
+<Navbar></Navbar>
+    </>
+  );
+}
+
+export default App;
